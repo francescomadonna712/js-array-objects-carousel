4ef81d03f44a0bab7bc7c9f4fe87b9456136861c
francesco madonna <fra.mad.93@hotmail.it>
2024-05-07 20:35:13 +0200

    commit per save

--- js/main.js
+++ js/main.js
@@ -21,3 +21,78 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const itemsCarousel = document.querySelector('.items-carousel');
+const btnUp = document.querySelector('.up');
+const btnDown = document.querySelector('.down');
+const thumbnails = document.querySelector('.thumbnails');
+
+let counter = 0;
+
+// svuoto elementi nell html
+itemsCarousel.innerHTML = '';
+thumbnails.innerHTML = '';
+
+
+images.forEach((element) => {
+    itemsCarousel.innerHTML += `
+  <div class = 'block-carousel newItem hide'>
+    <img src="${element.image}">
+    <div class = 'block-description'>
+      <h3>${element.title}</h3>
+      <p>${element.text}</p>
+    </div>
+  </div>`;
+    thumbnails.innerHTML += `<img src="${element.image}"class= "thumbItem inactive">`;
+})
+
+const newItem = document.getElementsByClassName('newItem');
+
+const thumbItem = document.getElementsByClassName('thumbItem')
+
+
+// prendi indice 0 del nuovo array
+newItem[0].classList.remove('hide');
+thumbItem[0].classList.add('active');
+
+
+// bottone down
+btnDown.addEventListener('click', function () {
+
+    addRemove();
+
+    counter++;
+
+    if (counter === newItem.length) counter = 0;
+
+    removeAdd();
+})
+
+
+
+// bottone  su
+btnUp.addEventListener('click', function () {
+
+    addRemove();
+
+    counter--;
+
+    if (counter < 0) counter = newItem.length - 1;
+
+    removeAdd();
+})
+
+
+
+// function add/remove
+function addRemove() {
+    newItem[counter].classList.add('hide');
+    thumbItem[counter].classList.remove('active');
+}
+
+
+// function remove/add
+function removeAdd() {
+    newItem[counter].classList.remove('hide');
+    thumbItem[counter].classList.add('active');
+}
